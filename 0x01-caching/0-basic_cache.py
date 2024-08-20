dfae38a7c204ecef489922925e9504238e177838
akorede ridwanullahi abiodun <akoredera@gmail.com>
2024-08-20 11:02:40 +0000

    add files

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,18 @@
+#!usr/bin/env python3
+'''0. Basic dictionary'''
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def __init__(self):
+        '''constructor inherit from the base'''
+        super().__init__()
+
+    def put(self, key, item):
+        '''must assign to the dictionary'''
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''must return the value in self.cache_data'''
+        return self.cache_data.get(key, None)
